eda3484699c5e5bc72cc6b7b3f53a536d59a1dd6
kuzGo <gorankuzma467@gmail.com>
2021-08-01 15:23:09 +0000

    Create base.html and static files, add Materialize

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, redirect,
+    render_template, request, url_for, session)
+from flask_pymongo import PyMongo
+from bson import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def test():
-    return "<h1>Testing the app</h1>"
+@app.route("/get_activities")
+def get_activities():
+    activities = mongo.db.activities.find()
+    return render_template("activities.html", activities=activities)
 
 
 if __name__ == "__main__":
