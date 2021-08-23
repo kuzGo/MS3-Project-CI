721bca02762ec1117443b213e6713bd8512f0905
kuzGo <gorankuzma467@gmail.com>
2021-08-23 21:38:29 +0000

    Add select dropdown input validation

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,23 @@ $(document).ready(function(){
     $('select').formSelect();
     $('.modal').modal();
     $('.tooltipped').tooltip();
+    // CREDIT FOR CODE http://jsfiddle.net/b8frk03m/6/
+    // for HTML5 "required" attribute
+    $("select[required]").css({
+      display: 'inline',
+      position: 'absolute',
+      float: 'left',
+      padding: 0,
+      margin: 0,
+      border: '1px solid rgba(255,255,255,0)',
+      height: 0, 
+      width: 0,
+      top: '2em',
+      left: '3em',
+      opacity: 0,
+      pointerEvents: 'none'
+    });
+    // End of credit
   });
 
 
